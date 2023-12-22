49ab80cbfffe7645bcecfa175102cf9b71fc91d9
Photon1c <lescua@gmail.com>
2023-12-22 09:46:52 -0800

    set port to listen to

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def converse():
         return jsonify({'error': str(e), 'traceback': traceback.format_exc()}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
